30b16ca02cb7e7188d908f7ea1b0128a0e3399ea
ryanthomas92 <rthoma11@villanova.edu>
2017-01-11 14:42:48 -0800

    created hangman

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,4 +2,15 @@ console.log('app.js loaded!');
 
 // initialize the application
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController );
+
+function HangmanController() {
+  var vm = this;
+
+  vm.hangman = new HangmanGame('elephant')
+  vm.guessLetter = function(input) {
+    vm.hangman.guess(input);
+    vm.hangman.input = "";
+  }
+}
